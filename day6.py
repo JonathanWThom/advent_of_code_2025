74043ef2a8c8cae9098846881a13cc66a6b7b2c9
Jonathan Thom <jonathanthom@hey.com>
2025-12-05 22:03:09 -0800

    eh

--- day6.py
+++ day6.py
@@ -1,21 +1,43 @@
-from functools import reduce
+from functools import reduce, cache
 
-lines = open("./day6_input.txt").read().split("\n")[:-1]
-split_lines = []
-for line in lines:
-    split = list(filter(None, line.split(" ")))
-    split_lines.append(split)
+class LineGetter:
+    def __init__(self, path):
+        self.path = path
 
-collections = []
-for i in range(0, len(split_lines[0])):
-    collection = []
-    for split in split_lines:
-        collection.append(split[i])
-    collections.append(collection)
+    def lines(self):
+        return open(self.path).read().split("\n")[:-1]
 
-grand_total = 0
-for collection in collections:
-    operator = collection.pop()
-    grand_total += reduce(lambda x, y: eval(f"{x} {operator} {y}"), collection)
+class Homework:
+    def __init__(self):
+        self.path = "./day6_input.txt"
 
-print(grand_total)
+    def solve(self):
+        grand_total = 0
+        for collection in self._collections():
+            operator = collection.pop()
+            grand_total += reduce(lambda x, y: eval(f"{x} {operator} {y}"), collection)
+
+        print(grand_total)
+        return grand_total
+
+    def _collections(self):
+        collections = []
+        for i in range(0, len(self._filtered_lines()[0])):
+            collection = []
+            for split in self._filtered_lines():
+                collection.append(split[i])
+            collections.append(collection)
+
+        return collections
+
+    @cache
+    def _filtered_lines(self):
+        def no_empties(line):
+            return list(filter(None, line.split(" ")))
+
+        return list(map(no_empties, self._lines()))
+
+    def _lines(self):
+        return LineGetter(self.path).lines()
+
+Homework().solve()
