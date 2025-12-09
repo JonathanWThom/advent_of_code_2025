cbb142e20c92915763323c94b74ab0276c8b5573
Jonathan Thom <jonathanthom@hey.com>
2025-12-08 22:10:49 -0800

    right idea but too slow

--- day9.py
+++ day9.py
@@ -41,79 +41,18 @@ class Floor:
     def get_best_rectangle_for(self, tile):
         areas = []
 
-        # horizontal
-        for x in range(0, self.width+1): # +2?
-            if x == tile.x:
+        for red_tile in self.red_tiles:
+            if red_tile == tile:
                 continue
-            if self.tiles[x][tile.y].red == True:
-                area = abs(x-tile.x)+1
-                print(f"tile at [{x}, {tile.y}] forms rectangle with [{tile.x}, {tile.y}] with area: {area}")
-                areas.append(area)
-
-        # vertical
-        for y in range(0, self.depth+1): # +2?
-            if y == tile.y:
-                continue
-
-            if self.tiles[tile.x][y].red == True:
-                area = abs(y-tile.y)+1
-                print(f"tile at [{tile.x}, {y}] forms rectangle with [{tile.x}, {tile.y}] with area: {area}")
-                areas.append(area)
-
-        # up right diagonally: wrong algo
-        for x in range(tile.x+1, self.width+1): # move to the right starting at tile until reaching end
-            current_y =
-            # for each move to the right, you have to subtract 1 from tile.y
-            if tile.x == 2 and tile.y == 5:
-                if x == 11:
-                    print("current_y", current_y)
-
-            if self.tiles.get(x, {}).get(current_y, Tile(-1,-1)).red == True:
-                x_move = abs(x-tile.x)+1
-                y_move = abs(curren_y-tile.y)+1
-                area = x_move * y_move
-                print(f"tile at [{x}, {current_y}] forms rectangle with [{tile.x}, {tile.y}] with area: {area}")
-                areas.append(area)
-
-        # down right diagonally: wrong algo
-        for x in range(tile.x, self.width+1): # move to the right starting at tile until reaching end
-            # for each move to the right, you have to add 1 to tile.y
-            current_y = tile.y + 1
-            if self.tiles.get(x, {}).get(current_y, Tile(-1,-1)).red == True:
-                x_move = abs(x-tile.x)+1
-                y_move = abs(curren_y-tile.y)+1
-                area = x_move * y_move
-                print(f"tile at [{x}, {current_y}] forms rectangle with [{tile.x}, {tile.y}] with area: {area}")
-                areas.append(area)
+            x_magnitude = abs(red_tile.x - tile.x)+1
+            y_magnitude = abs(red_tile.y - tile.y)+1
+            area = x_magnitude * y_magnitude
+            areas.append(area)
 
         return max(areas)
 
 f=Floor(width, depth, lines)
-print(len(f.red_tiles))
 for tile in f.red_tiles:
     areas.append(f.get_best_rectangle_for(tile))
 
-# rules about what coordinates form triangles with a coordinate
-# [0,0] [1,0] [2,0] [3,0] [4,0]
-# [0,1] [1,1] [2,1] [3,1] [4,1]
-# [0,2] [1,2] [2,2] [3,2] [4,2]
-# [0,3] [1,3] [2,3] [3,3] [4,3]
-# [0,4] [1,4] [2,4] [3,4] [4,4]
-
-# NO this is the wrong idea, it doesn't follow these 1 up, 1 to the side rules
-# for [2,2], possible thin rectangles are
-# anything with same y value that are not it: [0,2] [1,2] [3,2] [4,2]
-# anything with same x value that is not it: [2,0] [2,1] [2,3] [2,4]
-# up right diagonally: [3, 1] [4,0]
-# down right diagonally: [3, 3] [4,4]
-# down left diagonally: [1,3] [0,4]
-# up left diagonally: [1,1] [0,0]
-
-# fill in the floor with max x and max y in a dict
-# then go through and mark certain tiles as red (maybe make dict too?), or just
-# a list
-# then for each red tile, check its possible rectangles in the floor to see if
-# they are also red
-# if they are, get the area
-# collect all areas and get the max
 print(max(areas))
