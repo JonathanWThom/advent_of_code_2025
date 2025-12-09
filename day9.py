2b755d877f221414568b01ec2acb9878b5f60564
Jonathan Thom <jonathanthom@hey.com>
2025-12-08 22:22:35 -0800

    simplify a bit

--- day9.py
+++ day9.py
@@ -1,45 +1,29 @@
 lines = open("./day9_input.txt").read().splitlines()
 
-def x(line):
-    return int(line.split(",")[0])
-
-def y(line):
-    return int(line.split(",")[1])
-
-width = max(list(map(x, lines)))
-depth = max(map(y, lines))
 areas = []
 
 class Tile:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.matches = []
 
 class Floor:
-    def __init__(self, width, depth, lines):
-        self.width = width
-        self.depth = depth
-        self.red_tiles = []
-        self.tiles = self._build_tiles(lines)
+    def __init__(self, lines):
+        self.red_tiles = self._build_tiles(lines)
 
     def _build_tiles(self, lines):
-        tmp = dict()
-
+        tmp = []
         for line in lines:
             x, y = line.split(",")
             tile = Tile(int(x), int(y))
-            if tmp.get(int(x)) == None:
-                tmp[int(x)] = {}
-            tmp[int(x)][int(y)] = tile
-            self.red_tiles.append(tile)
+            tmp.append(tile)
 
         return tmp
 
     def get_best_rectangle_for(self, tile):
         areas = []
 
-        for i, red_tile in enumerate(self.red_tiles):
+        for red_tile in self.red_tiles:
             if red_tile == tile:
                 continue
 
@@ -53,7 +37,7 @@ class Floor:
 
         return max(areas)
 
-f=Floor(width, depth, lines)
+f=Floor(lines)
 for tile in f.red_tiles:
     areas.append(f.get_best_rectangle_for(tile))
 
